562307a9e02460696505e8795d48557192a4cf90
Ian Chen <ianre657@gmail.com>
2018-08-03 17:15:52 +0800

    multhread compiling & add build type

--- build.sh
+++ build.sh
@@ -36,29 +36,35 @@ git clone https://github.com/stevenlovegrove/Pangolin.git
 cd Pangolin
 mkdir build
 cd build
-cmake ../ -DAVFORMAT_INCLUDE_DIR="" -DCPP11_NO_BOOST=ON
-make -j8
+cmake ../
+make -j$(nproc) -l$(nproc)
 cd ../..
 
 #Up to date OpenNI2
 git clone https://github.com/occipital/OpenNI2.git
 cd OpenNI2
-make -j8
+make -j$(nproc) -l$(nproc)
 cd ..
+
+
+# build_type is Debug/Release
+BUILD_TYPE="Debug"
 
 #Actually build ElasticFusion
 cd ../Core
 mkdir build
 cd build
-cmake ../src
-make -j8
+cmake ../src -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+make -j$(nproc) -l$(nproc)
+
 cd ../../GPUTest
 mkdir build
 cd build
-cmake ../src
-make -j8
+cmake ../src -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+make -j$(nproc) -l$(nproc)
+
 cd ../../GUI
 mkdir build
 cd build
-cmake ../src
-make -j8
+cmake ../src -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+make -j$(nproc) -l$(nproc)
